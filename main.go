62b26db0d94afa4b4bb401f3dabe22c6a4bf1ddb
Colin Alston <colin.alston@gmail.com>
2016-09-07 14:10:14 +0200

    Base main.go

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "github.com/hashicorp/terraform/plugin"
+    "github.com/calston/terraform-provider-rhev/rhev"
+)
+
+func main() {
+    plugin.Serve(&plugin.ServeOpts{
+        ProviderFunc: rhev.Provider,
+    })
+}
